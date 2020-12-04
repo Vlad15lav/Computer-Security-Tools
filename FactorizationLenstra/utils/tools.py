11a53706b5bdcd0080532a34d79e359a411132ba
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2020-12-04 21:47:12 +0300

    Update tools.py

--- FactorizationLenstra/utils/tools.py
+++ FactorizationLenstra/utils/tools.py
@@ -9,35 +9,35 @@ def RandomCurve(n): # Random X Y A for Curve
     return random.randint(0, n), random.randint(0, n), random.randint(0, n)
   
 def MiillerRabin(d, n): # Test Miller Rabin on prime number
-    a = 2 + random.randint(1, n - 4); 
+    a = 2 + random.randint(1, n - 4)
   
     x = pow(a, d, n)
   
     if (x == 1 or x == n - 1): 
-        return True; 
+        return True
 
     while (d != n - 1): 
-        x = (x * x) % n; 
-        d *= 2; 
+        x = (x * x) % n
+        d *= 2 
   
         if (x == 1): 
-            return False; 
+            return False
         if (x == n - 1): 
-            return True; 
+            return True
   
-    return False;
+    return False
 
 def isPrime(n): # Check on prime number
     if (n <= 1 or n == 4): 
-        return False; 
+        return False
     if (n <= 3): 
-        return True; 
+        return True
   
     t = n - 1; 
     while (t % 2 == 0): 
-        t //= 2; 
+        t //= 2
 
     if (MiillerRabin(t, n) == False): 
-        return False; 
+        return False;
   
-    return True;
+    return True
